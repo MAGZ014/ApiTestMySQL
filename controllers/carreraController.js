76adced5993a6afbd58c15e42f01cd5318f70619
Manuel Alejandro <elgtorner10fega@gmail.com>
2024-10-21 21:37:54 -0600

    Controladores terminados

--- controllers/carreraController.js
+++ controllers/carreraController.js
@@ -7,13 +7,55 @@ import {
 
 export class CarreraController {
   //Obtener todos los clientes
-  static async getAll(req, res) {}
+  static async getAll(req, res) {
+    const carreras = await CarreraMadel.getAll();
+    res.json(carreras);
+  }
 
-  static async getById(req, res) {}
+  static async getById(req, res) {
+    const { id } = req.params;
+    const carrera = await CarreraMadel.getById({ id });
+    if (!carrera) {
+      return res.status(404).json({ message: "Carrera not found" });
+    }
+    res.json(carrera);
+  }
 
-  static async create(req, res) {}
+  static async create(req, res) {
+    const result = validateCarrera(req.body);
+    console.log(result);
+    if (!result.success) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) });
+    }
+    const newCarrera = await CarreraMadel.create({ input: result.data });
+    res.status(201).json(newCarrera);
+  }
 
-  static async delete(req, res) {}
+  static async delete(req, res) {
+    const { id } = req.params;
+    const result = await CarreraMadel.delete({ id });
 
-  static async update(req, res) {}
+    if (result === false) {
+      return res.status(404).json({ message: "Carrera not found" });
+    }
+
+    return res.json({ message: "Carrera deleted" });
+  }
+
+  static async update(req, res) {
+    const result = validatePartialCarrera(req.body);
+
+    if (!result.success) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) });
+    }
+
+    const { id } = req.params;
+
+    const updatedCarrera = await CarreraMadel.update({
+      id,
+      input: result.data,
+    });
+
+    return res.json(updatedCarrera);
+  }
 }
