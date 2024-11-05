31ea950f81f8f3da5ebd947d0faaaa853f4b9483
Manuel Alejandro <elgtorner10fega@gmail.com>
2024-11-04 19:44:52 -0600

    Encripta pass

---
+++ encry.js
@@ -0,0 +1,33 @@
+import { getConnection } from "./config/conexion.js";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+
+async function encryptPasswords() {
+  try {
+    const connection = await getConnection();
+
+    // Obtener todos los usuarios con contraseñas en texto plano
+    const [users] = await connection.query(
+      `SELECT id, password FROM user WHERE password NOT LIKE '$2y$%'`
+    );
+
+    for (const user of users) {
+      // Encriptar la contraseña
+      const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+
+      // Actualizar la base de datos con la contraseña encriptada
+      await connection.query(`UPDATE user SET password = ? WHERE id = ?`, [
+        hashedPassword,
+        user.id,
+      ]);
+      console.log(`Contraseña de usuario ${user.id} encriptada`);
+    }
+
+    console.log("Proceso de encriptación completado.");
+  } catch (error) {
+    console.error("Error en la encriptación de contraseñas:", error);
+  }
+}
+
+encryptPasswords();
